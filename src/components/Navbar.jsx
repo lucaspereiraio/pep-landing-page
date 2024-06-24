55af8aff58c1a2cdf7a4243177de209ab7382b81
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2024-06-24 16:36:36 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="text-black">
-      <img
+      {/* <img
         className="w-1"
         src="././assets/pep-logo.jpeg"
         alt="Logo do Parque Estadual dos Pireneus"
-      />
+      /> */}
       <h1 className="w-full text-1xl font-bold">
         Parque Estadual dos Pireneus
       </h1>
