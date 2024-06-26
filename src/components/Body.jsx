aec9dce7f4abae27b3f8535fc5106958f7ca5e71
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2024-06-26 16:05:00 -0300

    styling

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Body() {
+  return (
+    <div className="flex">
+      <img src="../assets/pep-logo.jpeg" alt="" className=" size-10" />
+      <div>hello</div>
+    </div>
+  );
+}
+
+export default Body;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import logo from "../assets/pep-oficial-logo.png";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -9,37 +10,45 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black">
-      {/* <img
-        className="w-1"
-        src="././assets/pep-logo.jpeg"
+    <div className="flex justify-between items-center h-12 max-w-[1240px] mx-auto px-4 py-2 text-black">
+      <img
+        className="size-10 mr-4"
+        src={logo}
         alt="Logo do Parque Estadual dos Pireneus"
-      /> */}
-      <h1 className="w-full text-xl font-bold text-[#005A50]">
+      />
+      <h1 className="w-full text-xl font-bold text-[#025949]">
         Parque Estadual dos Pireneus - PEP
       </h1>
       <ul className="hidden md:flex">
         <li className="p-4">Início</li>
         <li className="p-4">Sobre</li>
         <li className="p-4">Esportes</li>
+        <li className="p-4">Contato</li>
       </ul>
       <div onClick={handleNav} className="block md:hidden">
-        {!nav ? (
-          <AiOutlineClose size={20} color="#005A50" />
+        {nav ? (
+          <AiOutlineClose size={20} color="#025949" />
         ) : (
-          <AiOutlineMenu size={20} color="#005A50" />
+          <AiOutlineMenu size={20} color="#025949" />
         )}
       </div>
       <div
         className={
-          !nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r bg-[#86BF00] border-r-gray-900 ease-in-out duration-500"
+          nav
+            ? "fixed left-0 top-0 w-[70%] h-full border-r bg-[#b3d16a] border-r-gray-900 ease-in-out duration-500 md:hidden"
             : "fixed left-[-100%]"
         }
       >
-        <h1 className="w-full p-4 text-xl font-bold text-[#005A50]">
-          Parque Estadual dos Pirineus - PEP
-        </h1>
+        <div className="flex items-center py-1 px-4">
+          <img
+            className="size-10 mr-4"
+            src={logo}
+            alt="Logo do Parque Estadual dos Pireneus"
+          />
+          <h1 className="w-full text-xl font-bold text-[#025949]">
+            Parque Estadual dos Pireneus - PEP
+          </h1>
+        </div>
         <ul className="uppercase">
           <li className="p-4 border-b border-gray-600">Início</li>
           <li className="p-4 border-b border-gray-600">Sobre</li>
