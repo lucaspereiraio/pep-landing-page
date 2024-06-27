1b081bfe9c5c07c9bc88b1c19e8acd5b075f5f51
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2024-06-27 18:01:03 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="flex justify-between items-center h-12 max-w-[1240px] mx-auto px-4 py-2 text-black bg-[#b3d16a]">
+      <div>Logo completa</div>
+      <div>Contatos - insta e telefone</div>
+      <div>Localização</div>
+      <div>Feito por mim</div>
+    </div>
+  );
 }
 
 export default Footer;
