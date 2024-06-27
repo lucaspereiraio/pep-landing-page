0e6b8111ecef18751b0a8771437f3f51c30d9c96
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2024-06-27 18:03:21 -0300

    whatsapp

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Whatsapp from "./components/Whatsapp";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Home />
       <About />
       <Contact />
+      <Whatsapp />
       <Footer />
     </div>
   );
---
+++ src/components/Whatsapp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Whatsapp() {
+  return <div>{/* Whatsapp flutuante */}</div>;
+}
+
+export default Whatsapp;
