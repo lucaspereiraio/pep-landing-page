6ea7338166ff50833c8f84fcb503179bafb874c4
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2024-06-28 14:33:26 -0300

    Home-styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,21 +4,27 @@ import morroDoCabeludo from "../assets/morro-do-cabeludo.jpg";
 function Home() {
   return (
     <div
-      className="flex flex-col items-center justify-center w-full h-screen bg-cover bg-center text-center"
+      className="flex items-center justify-center max-w-[1240px] w-full h-screen mx-auto bg-cover bg-center"
       style={{ backgroundImage: `url(${morroDoCabeludo})` }}
     >
       {/* <div className="bg-black bg-opacity-50 flex-1"></div> */}
-      <div className="bg-black bg-opacity-50 p-8 rounded-lg text-white">
-        <h1 className="text-4xl font-bold">
+      <div className="bg-black bg-opacity-60 p-8 rounded-lg text-white text-center shadow-lg">
+        <h1 className="text-4xl md:text-5xl font-bold">
           Bem-vindo ao Parque Estadual dos Pireneus
         </h1>
-        <p className="mt-4 text-lg">
+        <p className="mt-4 text-lg md:text-xl">
           Um refúgio de biodiversidade e beleza natural no coração de Goiás.
         </p>
-        <ul className="mt-6 flex justify-between">
-          <li>Sobre</li>
-          <li>Esportes</li>
-          <li>Contato</li>
+        <ul className="mt-6 flex space-x-6 justify-center font-bold text-lg">
+          <li className="hover:text-[#b3d16a] cursor-pointer transition-colors duration-200">
+            Sobre
+          </li>
+          <li className="hover:text-[#b3d16a] cursor-pointer transition-colors duration-200">
+            Esportes
+          </li>
+          <li className="hover:text-[#b3d16a] cursor-pointer transition-colors duration-200">
+            Contato
+          </li>
         </ul>
       </div>
     </div>
